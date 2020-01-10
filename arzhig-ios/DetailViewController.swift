4b20ad755c6a04e4121c78fdbf717f5596a3aa3c
Mick F <mick@mickf.net>
2020-01-10 13:38:14 +0100

    feat: initial commit

---
+++ arzhig-ios/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  arzhig-ios
+//
+//  Created by Mickaël Floc'hlay on 10/01/2020.
+//  Copyright © 2020 mickf.net. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
