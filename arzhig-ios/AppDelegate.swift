0222e2b74b11466d8cd92ae3b884954314ace64e
Mick F <mick@mickf.net>
2020-01-24 13:45:10 +0100

    feat(play-videos): debug log: show path of NSHomeDirectory at startup

--- arzhig-ios/AppDelegate.swift
+++ arzhig-ios/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        #if DEBUG
+        debugPrint("Home directory is: \(NSHomeDirectory())")
+        #endif
+
         self.coordinator = VideosCoordinator()
         self.coordinator?.start()
         
