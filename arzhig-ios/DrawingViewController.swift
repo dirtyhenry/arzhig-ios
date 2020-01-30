00c5cf0125c138d35088dd2affd483b51dd533b4
Mick F <mick@mickf.net>
2020-01-30 18:29:13 +0100

    fix(drawing): fix sample code

--- arzhig-ios/DrawingViewController.swift
+++ arzhig-ios/DrawingViewController.swift
@@ -26,7 +26,7 @@ class DrawingViewController: UIViewController {
             mainImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             mainImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             mainImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-
+            
             tempImageView.topAnchor.constraint(equalTo: mainImageView.topAnchor),
             tempImageView.leadingAnchor.constraint(equalTo: mainImageView.leadingAnchor),
             tempImageView.trailingAnchor.constraint(equalTo: mainImageView.trailingAnchor),
@@ -35,67 +35,67 @@ class DrawingViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-          guard let touch = touches.first else {
+        guard let touch = touches.first else {
             return
-          }
-          swiped = false
-          lastPoint = touch.location(in: view)
+        }
+        swiped = false
+        lastPoint = touch.location(in: mainImageView)
     }
     
     func drawLine(from fromPoint: CGPoint, to toPoint: CGPoint) {
-      // 1
-      UIGraphicsBeginImageContext(view.frame.size)
-      guard let context = UIGraphicsGetCurrentContext() else {
-        return
-      }
-      tempImageView.image?.draw(in: view.bounds)
+        // 1
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        tempImageView.image?.draw(in: mainImageView.bounds)
         
-      // 2
-      context.move(to: fromPoint)
-      context.addLine(to: toPoint)
-      
-      // 3
-      context.setLineCap(.round)
-      context.setBlendMode(.normal)
-      context.setLineWidth(brushWidth)
-      context.setStrokeColor(color.cgColor)
-      
-      // 4
-      context.strokePath()
-      
-      // 5
-      tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-      tempImageView.alpha = opacity
-      UIGraphicsEndImageContext()
+        // 2
+        context.move(to: fromPoint)
+        context.addLine(to: toPoint)
+        
+        // 3
+        context.setLineCap(.round)
+        context.setBlendMode(.normal)
+        context.setLineWidth(brushWidth)
+        context.setStrokeColor(color.cgColor)
+        
+        // 4
+        context.strokePath()
+        
+        // 5
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
     }
-
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-      guard let touch = touches.first else {
-        return
-      }
-
-      // 6
-      swiped = true
-      let currentPoint = touch.location(in: view)
-      drawLine(from: lastPoint, to: currentPoint)
+        guard let touch = touches.first else {
+            return
+        }
         
-      // 7
-      lastPoint = currentPoint
+        // 6
+        swiped = true
+        let currentPoint = touch.location(in: mainImageView)
+        drawLine(from: lastPoint, to: currentPoint)
+        
+        // 7
+        lastPoint = currentPoint
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-      if !swiped {
-        // draw a single point
-        drawLine(from: lastPoint, to: lastPoint)
-      }
+        if !swiped {
+            // draw a single point
+            drawLine(from: lastPoint, to: lastPoint)
+        }
         
-      // Merge tempImageView into mainImageView
-      UIGraphicsBeginImageContext(mainImageView.frame.size)
-      mainImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: 1.0)
-      tempImageView.image?.draw(in: view.bounds, blendMode: .normal, alpha: opacity)
-      mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
-      UIGraphicsEndImageContext()
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        mainImageView.image?.draw(in: mainImageView.bounds, blendMode: .normal, alpha: 1.0)
+        tempImageView.image?.draw(in: mainImageView.bounds, blendMode: .normal, alpha: opacity)
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
         
-      tempImageView.image = nil
+        tempImageView.image = nil
     }
 }
