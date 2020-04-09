832d130dba0db281558e2b45521ac834a4023c57
Mick F <mick@mickf.net>
2020-04-09 15:40:03 +0200

    feat(parents): add target for parents app

---
+++ arzhig-parents-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  arzhig-parents-ios
+//
+//  Created by Mickaël Floc'hlay on 09/04/2020.
+//  Copyright © 2020 mickf.net. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
