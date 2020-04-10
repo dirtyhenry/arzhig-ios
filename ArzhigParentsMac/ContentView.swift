d967a0afa93b6942fe7e40fd926a20f8c345a4cd
Mick F <mick@mickf.net>
2020-04-10 18:07:39 +0200

    feat: add scaffold for macOS app for parents

---
+++ ArzhigParentsMac/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ArzhigParentsMac
+//
+//  Created by Mickaël Floc'hlay on 10/04/2020.
+//  Copyright © 2020 mickf.net. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
