1a027476b24ae00dbf4a451366eeddca154ca9f2
Mick F <mick@mickf.net>
2020-07-24 11:44:58 +0200

    feat(location): scaffold tab view adding location view

---
+++ ArzhigParents/LocationView/LocationView.swift
@@ -0,0 +1,21 @@
+//
+//  LocationView.swift
+//  ArzhigParents
+//
+//  Created by Mickaël Floc'hlay on 24/07/2020.
+//  Copyright © 2020 mickf.net. All rights reserved.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text("Location")
+    }
+}
+
+struct LocationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationView()
+    }
+}
--- ArzhigParents/SceneDelegate.swift
+++ ArzhigParents/SceneDelegate.swift
@@ -1,26 +1,38 @@
-//
-//  SceneDelegate.swift
-//  arzhig-parents-ios
-//
-//  Created by Mickaël Floc'hlay on 09/04/2020.
-//  Copyright © 2020 mickf.net. All rights reserved.
-//
-
 import UIKit
 import SwiftUI
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+struct AppView: View {
+    var body: some View {
+        TabView {
+            RainForecastList()
+                .tabItem {
+                    Image(systemName: "cloud.sun")
+                    Text("Rain")
+            }
+            
+            LocationView()
+                .tabItem {
+                    Image(systemName: "location")
+                    Text("Location")
+            }
+        }
+    }
+}
 
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = RainForecastList()
+        let contentView = AppView()
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
